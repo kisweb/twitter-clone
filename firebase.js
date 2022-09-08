44a2ed5739857501bfc29a812c9108573e34278d
Ibrahima Sarr <kisarrweb@gmail.com>
2022-09-08 14:12:10 +0000

    all done

--- firebase.js
+++ firebase.js
@@ -7,12 +7,18 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
-  authDomain: "twitter-v4.firebaseapp.com",
-  projectId: "twitter-v4",
-  storageBucket: "twitter-v4.appspot.com",
-  messagingSenderId: "958780669916",
-  appId: "1:958780669916:web:c0ab24c3f605b0a233d919",
+  apiKey: "AIzaSyDIvfrJ-awY0V3XRnzzuMRVYjIqteq7udg",
+  authDomain: "kis-twitter.firebaseapp.com",
+  projectId: "kis-twitter",
+  storageBucket: "kis-twitter.appspot.com",
+  messagingSenderId: "230906014107",
+  appId: "1:230906014107:web:c30dde99304a06ec65d93e"
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+  // authDomain: "twitter-v4.firebaseapp.com",
+  // projectId: "twitter-v4",
+  // storageBucket: "twitter-v4.appspot.com",
+  // messagingSenderId: "958780669916",
+  // appId: "1:958780669916:web:c0ab24c3f605b0a233d919",
 };
 
 // Initialize Firebase
